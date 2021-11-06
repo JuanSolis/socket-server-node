0fc19feca040869269c5db32ce13e50d04e668ae
JuanSolis <juansolisgtsk@hotmail.com>
2021-11-06 09:36:46 -0600

    Union de backend con modulo wifi

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ server.listen(3200, () => {
 var io = require('socket.io')(server, {cors: {origin: "*"}});
 
 io.on('connection', function(socket){
-	console.log("----Cliente Conectado----");
-	socket.on('stateChanged', function(state){
-        console.log("Data mandada por Form de React")
-		io.emit("updateState", state);
+	  socket.on('sendActivation', function(state){
+		  io.emit("sendActivationToClients", state);
+      console.log("Conectado")
 	});
+ 
 });
 
 var admin = require("firebase-admin");
-
+var constValueMod = "False";
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -44,11 +44,11 @@ const db = admin.firestore();
 
 app.get('/', (req, res) => {
     let db_response = [];
-    db.collection("Data_Response").get().then((querySnapshot) => {
+    db.collection("num_hexadecimales").get().then((querySnapshot) => {
         querySnapshot.docs.map((doc) => {
             const element = {};
             element.id = doc.id;
-            element.mensaje = doc._fieldsProto.mensaje.stringValue;
+            element.hexa = doc._fieldsProto.hexa.stringValue;
             db_response.push(element);
         });
         res.json(db_response);
@@ -57,10 +57,33 @@ app.get('/', (req, res) => {
   });
 
   app.post('/post_data',jsonParser, function(req, res) {
-    const mensaje = {"mensaje": ""};
-    const dataparam = req.body.postDataParam;
-    mensaje.mensaje = dataparam;
-    db.collection("Data_Response").doc().set(mensaje)
+    const mensaje = {"hexa": ""};
+    const dataparam = req.body.hexa;
+    mensaje.hexa = dataparam;
+    db.collection("num_hexadecimales").doc().set(mensaje)
   });
+
+  app.post('/data_rasp',jsonParser, function(req, res) {
+    const activacionRasp = {"rasp_Activation": ""};
+    const dataparam_Rasp = req.body.rasp_Activation;
+    activacionRasp.rasp_Activation = dataparam_Rasp;
+    console.log(dataparam_Rasp);
+    //db.collection("num_hexadecimales").doc().set(mensaje)
+  });
+
+  app.post('/data_ModWifi',jsonParser, function(req, res) {
+    const activacionModWifi = {"modWifi_Activation": ""};
+    const dataparam_ModWifi = req.body.modWifi_Activation;
+    activacionModWifi.modWifi_Activation = dataparam_ModWifi;
+    console.log(dataparam_ModWifi);
+    constValueMod = dataparam_ModWifi;
+    //db.collection("num_hexadecimales").doc().set(mensaje)
+  });
+
+  app.get('/data_ModWifi',jsonParser, function(req, res) {
+    const dataparam_ModWifi = res.json({constValueMod});
+    //db.collection("num_hexadecimales").doc().set(mensaje)
+  });
+  
   
   
